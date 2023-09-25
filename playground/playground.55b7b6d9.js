2f174388503003bd1f45c1d66efc80f6dc7096d4
Phillip Rearick <psrearick@gmail.com>
2023-09-25 12:08:12 -0400

    wip

--- playground/playground.55b7b6d9.js
+++
@@ -1,6 +0,0 @@
-!// Avoid `console` errors in browsers that lack a console.
-function(){for(var e,n=function(){},o=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeline","timelineEnd","timeStamp","trace","warn"],i=o.length,r=window.console=window.console||{};i--;)// Only stub undefined methods.
-r[e=o[i]]||(r[e]=n)}();// Place any jQuery/helper plugins in here.
-//# sourceMappingURL=playground.55b7b6d9.js.map
-
-//# sourceMappingURL=playground.55b7b6d9.js.map
