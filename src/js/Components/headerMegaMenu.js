12501934ce2449fa3b4b2b28a7c57d3d15ee8d81
Phillip Rearick <psrearick@gmail.com>
2023-09-28 19:42:19 -0400

    wip

--- src/js/Components/headerMegaMenu.js
+++ src/js/Components/headerMegaMenu.js
@@ -5,7 +5,6 @@ const checkbox = document.querySelector(
 );
 
 checkbox.addEventListener('change', function () {
-  console.log(this.checked);
   if (this.checked) {
     navbar.setAttribute('data-checked', '');
   } else {
@@ -15,7 +14,6 @@ checkbox.addEventListener('change', function () {
 
 navbar.querySelectorAll('li, button').forEach((element) => {
   element.addEventListener('click', (event) => {
-    console.log(event.target);
     if (event.target.closest('.noclick')) {
       return;
     }
@@ -32,6 +30,55 @@ navbar.querySelectorAll('li.menu').forEach((title) => {
     const isActive = listItem.classList.contains('active');
     const hiddenIcon = listItem.querySelector('svg.hidden');
     const regularIcon = listItem.querySelector('svg:not(.hidden)');
+
+    const dataMenuItems = Array.from(
+      navbar.querySelectorAll('[data-menu-content]'),
+    );
+
+    if (dataMenuItems.length > 0 && !isActive) {
+      navbar.setAttribute('data-active', '');
+    }
+
+    listItem.querySelectorAll('[data-menu-content]').forEach((content) => {
+      const id = content.getAttribute('data-menu-content');
+      const container = content.closest('[data-menu-container="' + id + '"]');
+
+      if (container && isActive) {
+        container.innerHTML = '';
+        return;
+      }
+
+      navbar
+        .querySelectorAll('[data-menu-container="' + id + '"]')
+        .forEach((container) => {
+          if (!isActive && container.innerHTML === '') {
+            container.append(content.cloneNode(true));
+            const child = container.lastChild;
+            child.classList.remove('hidden');
+            child.classList.add(child.getAttribute('data-active-display'));
+          }
+
+          const contentContainer = container.closest(
+            '.hidden-content-container',
+          );
+
+          if (!contentContainer) {
+            return;
+          }
+
+          if (isActive) {
+            contentContainer.classList.remove('active');
+
+            return;
+          }
+
+          contentContainer.classList.add('active');
+        });
+    });
+
+    if (dataMenuItems.length > 0 && isActive) {
+      setTimeout(() => navbar.removeAttribute('data-active'), 300);
+    }
 
     regularIcon.classList.add('hidden');
     hiddenIcon.classList.remove('hidden');
