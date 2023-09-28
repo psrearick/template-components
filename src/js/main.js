a12a960a5c352187f58e0603aaa3ce1cc7618d1d
Phillip Rearick <psrearick@gmail.com>
2023-09-27 21:37:56 -0400

    wip

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-// import PageBuilder from './pageBuilder';
 import App from './app';
 import loadFonts from './loadFonts';
 import { addContainerQueries } from './addContainerQueries';
@@ -6,12 +5,11 @@ import PageBuilder from './pageBuilder';
 
 export const run = async (definitions) => {
   const app = new App({ definitions });
-
   const pageBuilder = new PageBuilder(app.generator);
+
   await app.generator.createSections();
   await app.generator.addSectionsToDom();
   await addContainerQueries();
-
   await pageBuilder.registerListeners();
   await loadFonts(document);
 
